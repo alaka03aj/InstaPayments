8648a50ec315250ff88c1a621d6ee032a3621cc7
Alaka A J <alaka03aj@gmail.com>
2024-03-15 22:32:54 +0530

    Added: Payment base structure

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -1,17 +1,42 @@
-"use client";
+import { useState } from "react";
 import { useOrderContext } from "@/context/OrderContext";
-export default function Payment() {
 
+export default function Payment() {
     const { orderDetails, loading } = useOrderContext();
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+
     if (loading) {
         return <p>Loading...</p>;
     }
 
-    console.log(orderDetails)
-  return (
-    <>
-      <div>Payment Dashboard</div>
-      <p>Choose your payment method</p>
-    </>
-  );
+    const handlePaymentMethodChange = (event) => {
+        setSelectedPaymentMethod(event.target.value);
+    };
+
+    const payments = orderDetails.paymentMethods;
+
+    return (
+        <>
+            <div>Payment Dashboard</div>
+            <p>Choose your payment method</p>
+            <div>
+                {payments.map((payment) => (
+                    <div key={payment} style={{ display: "flex", gap: "32px" }}>
+                        <input
+                            type="radio"
+                            id={payment}
+                            name="paymentMethod"
+                            value={payment}
+                            checked={selectedPaymentMethod === payment}
+                            onChange={handlePaymentMethodChange}
+                        />
+                        <label htmlFor={payment}>{payment}</label>
+                    </div>
+                ))}
+            </div>
+            <button onClick={() => console.log("Selected payment method:", selectedPaymentMethod)}>
+                Confirm Payment
+            </button>
+        </>
+    );
 }
